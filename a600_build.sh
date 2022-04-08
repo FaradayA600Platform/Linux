326b2c3f224cfe1c2b2cc2153993295e4029b566
fred <fred@faraday-tech.com>
2022-04-08 11:58:18 +0800

    Modify faraday-a600.dts for 0x28030010

--- a600_build.sh
+++ a600_build.sh
@@ -1,3 +1,4 @@
 #make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- -j2
 ../uboot/tools/mkimage -A arm64 -O linux -C none -T kernel -a 0x83080000 -e 0x83080000 -n 'Linux-5.10.136' -d arch/arm64/boot/Image arch/arm64/boot/uImage
 #aarch64-linux-gnu-objdump -S -d -g -t vmlinux > kernel.asm
+cp arch/arm64/boot/dts/faraday/faraday-a600.dtb arch/arm64/boot/
